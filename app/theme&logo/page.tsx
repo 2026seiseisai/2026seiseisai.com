62e627fd01340c4d7740090939c23fe65ecf58d7
3D15Kiyama <kiyamakisuke.0708.kk@gmail.com>
2026-08-19 19:34:24 +0900

    ダウンロード可能化、レスポンシブ化

--- app/theme&logo/page.tsx
+++ app/theme&logo/page.tsx
@@ -7,7 +7,24 @@ import LogoMono from "./Logo-mono.svg";
 import LogoColor from "./Logo-color.svg";
 import doubleMono from "./double-mono.svg";
 import doubleColor from "./double-color.svg";
-import touch from "./Rectangle 1.svg";
+
+function DownloadButton({
+  file,
+  fileName,
+  className,
+}: {
+  file: { src: string };
+  fileName: string;
+  className: string;
+}) {
+  const downloadName = fileName.replace(/-D(?=\.[^.]+$)/, '');
+
+  return (
+    <a href={file.src} download={downloadName} className={`${styles.downloadButton} ${className}`}>
+      <Image src={downloadPicture} alt="Download" width={38} height={38} />
+    </a>
+  );
+}
 
 export default function App() {
   return (
@@ -25,30 +42,24 @@ export default function App() {
         <div className={styles.rectangle7} aria-hidden="true" />
         <h2 className={`${styles.sectionTitle} ${styles.title1}`}>Logotype</h2>
         <Image src={doubleColor} alt="Logo and Inf Color" className={styles.doubleColor} width={770} height={215} />
-        <Image src={downloadPicture} alt="Download" className={styles.doubleColor_D} width={38} height={38} />
+        <DownloadButton file={doubleColor} fileName="double-color-D.svg" className={styles.doubleColor_D} />
         <Image src={doubleMono} alt="Logo and Inf Mono" className={styles.doubleMono} width={770} height={215} />
-        <Image src={downloadPicture} alt="Download" className={styles.doubleMono_D} width={38} height={38} />
+        <DownloadButton file={doubleMono} fileName="double-mono-D.svg" className={styles.doubleMono_D} />
         <p className={`${styles.sectionText} ${styles.text1}`}>第62回菁々祭のテーマは“Infinity”です。生徒が持つ、無限の可能性を十分に発揮して欲しいという思いが込められています。</p>
 
         <h2 className={`${styles.sectionTitle} ${styles.title2}`}>Logo</h2>
         <Image src={LogoColor} alt="Logo Color" className={styles.LogoColor} width={332} height={220} />
-        <Image src={downloadPicture} alt="Download" className={styles.LogoColor_D} width={38} height={38} />
+        <DownloadButton file={LogoColor} fileName="Logo-color-D.svg" className={styles.LogoColor_D} />
         <Image src={LogoMono} alt="Logo Mono" className={styles.LogoMono} width={332} height={220} />
-        <Image src={downloadPicture} alt="Download" className={styles.LogoMono_D} width={38} height={38} />
+        <DownloadButton file={LogoMono} fileName="Logo-mono-D.svg" className={styles.LogoMono_D} />
         <p className={`${styles.sectionText} ${styles.text2}`}>テーマの“∞”を中心に据えながら、東大寺学園が100周年を迎えることにちなみ、100を思わせるシェイプになっています。</p>
 
         <h2 className={`${styles.sectionTitle} ${styles.title3}`}>Typo</h2>
         <Image src={InfColor} alt="Inf Color" className={styles.InfColor} width={334} height={137} />
-        <Image src={downloadPicture} alt="Download" className={styles.InfColor_D} width={38} height={38} />
+        <DownloadButton file={InfColor} fileName="Inf-color-D.svg" className={styles.InfColor_D} />
         <Image src={InfMono} alt="Inf Mono" className={styles.InfMono} width={334} height={137} />
-        <Image src={downloadPicture} alt="Download" className={styles.InfMono_D} width={38} height={38} />
+        <DownloadButton file={InfMono} fileName="Inf-mono-D.svg" className={styles.InfMono_D} />
         <p className={`${styles.sectionText} ${styles.text3}`}>太さを一定にすることで安定感を持たせ、文字の一部を繋げることで“続く”イメージを可視化しています。</p>
-
-        <h2 className={`${styles.sectionTitle} ${styles.title4}`}>Guideline</h2>
-        <Image src={touch} alt="touch" className={styles.touch} width={280} height={46} />
-        <p className={styles.textLink}>infinity_guideline.pdf</p>
-        <Image src={downloadPicture} alt="Download" className={styles.touch_D} width={38} height={38} />
-        <p className={`${styles.sectionText} ${styles.text4}`}>ロゴの使用にあたってはこちらのPDFをよく読み、内容を遵守していただくようお願いします。</p>
       </div>
     </div>
   );

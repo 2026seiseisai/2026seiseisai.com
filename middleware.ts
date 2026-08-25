98a289ae253621101b0684966e97b3541d7d3f58
mofmof2974 <leonmail2974@gmail.com>
2026-08-25 23:48:37 +0900

    theme&logo表示

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,7 @@ export function middleware(request: NextRequest) {
     pathname === '/news' ||
     pathname.startsWith('/news/') ||
     pathname === '/access' ||
+    pathname === '/theme&logo' ||
     pathname === '/404dummy'
   ) {
     return NextResponse.next();

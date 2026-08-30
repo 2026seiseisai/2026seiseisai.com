a06697512ba1f32d4240e65708aa74a17ed0ec56
mofmof2974 <leonmail2974@gmail.com>
2026-08-30 17:55:47 +0900

    exhibitions準備

---
+++ app/exhibitions/list/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function ExhibitionsListPage() {
+  return (
+    <main className={styles.page}>
+      <h1>Exhibition一覧</h1>
+    </main>
+  );
+}

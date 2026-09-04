bd5823f2e8404db51ad702f1013921dc0e0dfed7
Cry0lit3 <yuto-iguchi@outlook.com>
2026-09-04 21:59:02 +0900

    contact page

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
     pathname.startsWith('/news/') ||
     pathname === '/events'||
     pathname === '/access' ||
+    pathname === '/contact' ||
     pathname === '/theme&logo' ||
     pathname === '/special' ||
     pathname === '/exhibition-award' ||

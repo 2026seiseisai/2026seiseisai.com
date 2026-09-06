974f67acb6f9dcdafcdeca3e6fff9284865cd304
Fukumichi <fukumichikuroda@gmail.com>
2026-09-06 23:12:26 +0900

    Ninfinity迷路の時刻修正

--- app/events/event-data.ts
+++ app/events/event-data.ts
@@ -771,8 +771,8 @@ export const eventData: EventData[] = [
     description:
       '9時から14時30分まで、30分ごとの入場です。それぞれ約1時間周り、グラウンドにて整理券の配布を行います。',
     ticket: true,
-    day1: [{ location: '3年E組', start: '09:00', end: '15:00' }],
-    day2: [{ location: '3年E組', start: '09:00', end: '15:00' }],
+    day1: [{ location: '3年E組', start: '10:30', end: '15:00' }],
+    day2: [{ location: '3年E組', start: '09:00', end: '14:30' }],
   },
   {
     name: 'ラジコンレース大会',

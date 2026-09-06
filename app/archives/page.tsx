b3e4ee50107df101af5493837eec9b0a909e2f76
ryosuke051301 <ryosuke051301@gmail.com>
2026-09-06 20:22:40 +0900

    archieves作成

--- app/archives/page.tsx
+++ app/archives/page.tsx
@@ -1,5 +1,396 @@
-﻿import NotFoundDummyPage from '../404dummy/page';
+﻿import styles from './page.module.css';
+import Image from 'next/image';
+import logo_2025 from './2025.png';
+import logo_2024 from './2024.png';
+import logo_2023 from './2023.png';
+import logo_2022 from './2022.png';
+import logo_2021 from './2021.png';
+import logo_2020 from './2020.png';
+import logo_2019 from './2019.png';
+import logo_2017 from './2017.png';
+import logo_2016 from './2016.png';
+export default function Archives() {
+  return (
+    <>
 
-export default function ArchivesPage() {
-  return <NotFoundDummyPage />;
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Archives
+        </h1>
+        <p className={styles.explain}>ここでは過去の菁々祭のホームページを閲覧することができます。
+        </p>
+        <p className={styles.explain}>※一部のページはリンクが切れている場合があります。 予めご了承ください。
+        </p>
+        <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第61回-
+              <a className={`${styles.year} ${styles.color2025} `} href='https://2026seiseisai.pages.dev/2025'>
+                分秒
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2025' className={`${styles.color2025} ${styles.link} ${styles.color_button2025}`}>
+                https://2026seiseisai.pages.dev/2025
+              </a>
+            </div>
+          </h2>
+        <div>
+            
+          </div>
+          <Image
+        src={logo_2025}
+        alt="61th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+        <br />
+        <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第60回-
+              <a className={`${styles.year} ${styles.color2024}`} href='https://2026seiseisai.pages.dev/2024'>
+                IGNITION
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2024' className={`${styles.color2024} ${styles.link} ${styles.color_button2024}`}>
+                https://2026seiseisai.pages.dev/2024
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2024}
+        alt="60th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第59回-
+              <a className={`${styles.year} ${styles.color2023}`} href='https://2026seiseisai.pages.dev/2023'>
+                RESONANCE
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2023' className={`${styles.color2023} ${styles.link} ${styles.color_button2023}`}>
+                https://2026seiseisai.pages.dev/2023
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2023}
+        alt="59th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第58回-
+              <a className={`${styles.year} ${styles.color2022}`} href='https://2026seiseisai.pages.dev/2022'>
+                四季彩
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2022' className={`${styles.color2022} ${styles.link} ${styles.color_button2022}`}>
+                https://2026seiseisai.pages.dev/2022
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2022}
+        alt="58th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第57回-
+              <a className={`${styles.year} ${styles.color2021}`} href='https://2026seiseisai.pages.dev/2021'>
+                あをによし
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2021' className={`${styles.color2021} ${styles.link} ${styles.color_button2021}`}>
+                https://2026seiseisai.pages.dev/2021
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2021}
+        alt="57th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第56回-
+              <a className={`${styles.year} ${styles.color2020}`} href='https://2026seiseisai.pages.dev/2020'>
+                bright
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2020' className={`${styles.color2020} ${styles.link} ${styles.color_button2020}`}>
+                https://2026seiseisai.pages.dev/2020
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2020}
+        alt="56th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第55回-
+              <a className={`${styles.year} ${styles.color2019}`} href='https://2026seiseisai.pages.dev/2019'>
+                sailing
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2019' className={`${styles.color2019} ${styles.link} ${styles.color_button2019}`}>
+                https://2026seiseisai.pages.dev/2019
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2019}
+        alt="55th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第53回-
+              <a className={`${styles.year} ${styles.color2017}`} href='https://2026seiseisai.pages.dev/2017'>
+                自由楽果
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2017' className={`${styles.color2017} ${styles.link} ${styles.color_button2017}`}>
+                https://2026seiseisai.pages.dev/2017
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2017}
+        alt="53th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第52回-
+              <a className={`${styles.year} ${styles.color2016}`} href='https://2026seiseisai.pages.dev/2016'>
+                GO Together
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2016' className={`${styles.color2016} ${styles.link} ${styles.color_button2016}`}>
+                https://2026seiseisai.pages.dev/2016
+              </a>
+            </div>
+          </h2>
+          <div>
+            
+          </div>
+          <Image
+        src={logo_2016}
+        alt="52th"
+        className={styles.logo}
+        width={900}
+        height={215}
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+        <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第51回-
+              <a className={`${styles.year} ${styles.color2015}`} href='https://web.archive.org/web/20150915024420/http://www.seisei51st.com:80/'>
+                Re:Member
+              </a>
+              <br />
+              <a href='https://web.archive.org/web/20150915024420/http://www.seisei51st.com:80/' className={`${styles.color2015} ${styles.link} ${styles.color_button2015}`}>
+                http://www.seisei51st.com:80/(Internet Archive)
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第49回-
+              <a className={`${styles.year} ${styles.color2013}`} href='https://2026seiseisai.pages.dev/2013'>
+                漢度良好
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2013' className={`${styles.color2013} ${styles.link} ${styles.color_button2013}`}>
+                https://2026seiseisai.pages.dev/2013
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第48回-
+              <a className={`${styles.year} ${styles.color2012}`} href='https://2026seiseisai.pages.dev/2012'>
+                流星〜二日間の輝き〜
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2012' className={`${styles.color2012} ${styles.link} ${styles.color_button2012}`}>
+                https://2026seiseisai.pages.dev/2012
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第47回-
+              <a className={`${styles.year} ${styles.color2011}`} href='https://2026seiseisai.pages.dev/2011'>
+                刻〜EndloseZeit
+              </a>
+              <br />
+              <a href='https://2026seiseisai.pages.dev/2011' className={`${styles.color2011} ${styles.link} ${styles.color_button2011}`}>
+                https://2026seiseisai.pages.dev/2011
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第45回-
+              <a className={`${styles.year} ${styles.color2009}`} href='https://web.archive.org/web/20090909062151/http://www.seisei45.org/home.html'>
+                行け!!Men!!
+              </a>
+              <br />
+              <a href='https://web.archive.org/web/20090909062151/http://www.seisei45.org/home.html' className={`${styles.color2009} ${styles.link} ${styles.color_button2009}`}>
+                http://www.seisei45.org/home.html(Internet Archive)
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第44回-
+              <a className={`${styles.year} ${styles.color2008}`} href='https://web.archive.org/web/20080918072948/http://seiseisai44.net/'>
+                ただいまあきない中~無法地帯へようこそ~
+              </a>
+              <br />
+              <a href='https://web.archive.org/web/20080918072948/http://seiseisai44.net/' className={`${styles.color2008} ${styles.link} ${styles.color_button2008}`}>
+                http://seiseisai44.net/(Internet Archive)
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+        <br />
+         <div className={styles.background}>
+          <h2>
+            <div className={styles.center}>
+              第38回-
+              <a className={`${styles.year} ${styles.color2002}`} href='https://web.archive.org/web/20020808060607/http://seisei.info/tdj/'>
+                101%
+              </a>
+              <br />
+              <a href='https://web.archive.org/web/20020808060607/http://seisei.info/tdj/' className={`${styles.color2002} ${styles.link} ${styles.color_button2002}`}>
+                http://seisei.info/tdj/(Internet Archive)
+              </a>
+            </div>
+          </h2>
+          <br />
+        </div>
+      </main>
+    </>
+  );
 }
+
+
+
+
+
+
+
+

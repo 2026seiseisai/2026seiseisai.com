7bee99401ea6ef290cf4148133261a41680ec578
Fukumichi <fukumichikuroda@gmail.com>
2026-09-06 23:15:55 +0900

    Ninfinity迷路の説明

--- app/events/event-data.ts
+++ app/events/event-data.ts
@@ -769,7 +769,7 @@ export const eventData: EventData[] = [
   {
     name: '忍finity迷路',
     description:
-      '9時から14時30分まで、30分ごとの入場です。それぞれ約1時間周り、グラウンドにて整理券の配布を行います。',
+      '9時から14時30分まで、30分ごとの入場です。それぞれ約1時間前に、グラウンドにて整理券の配布を行います。',
     ticket: true,
     day1: [{ location: '3年E組', start: '10:30', end: '15:00' }],
     day2: [{ location: '3年E組', start: '09:00', end: '14:30' }],

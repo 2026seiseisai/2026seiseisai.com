164eaf5236cdb49950b6b96fb1247022db43431f
mofmof2974 <leonmail2974@gmail.com>
2026-09-07 23:47:13 +0900

    変更

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,6 @@ export function middleware(request: NextRequest) {
     pathname === '/news' ||
     pathname.startsWith('/news/') ||
     pathname === '/events' ||
-    pathname === '/goods' ||
     pathname === '/access' ||
     pathname === '/contact' ||
     pathname === '/privacy-policy' ||
